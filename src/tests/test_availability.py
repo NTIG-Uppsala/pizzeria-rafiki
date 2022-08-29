692dcd5edd713e08bc09460a960d214096e14197
Linus J <linus.jansson2000@gmail.com>
2022-08-29 13:07:59 +0200

    Kommentarer i testskript

--- src/tests/test_availability.py
+++ src/tests/test_availability.py
@@ -8,20 +8,20 @@ from webdriver_manager.core.utils import ChromeType
 
 class CheckSiteAvailability(unittest.TestCase):
     def setUp(self):
-        driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install() # installerar drivern i minnet
 
-        chrome_options = Options()
-        chrome_options.add_argument("--headless") #
+        chrome_options = Options() # 
+        chrome_options.add_argument("--headless") # 
 
-        self.browser = webdriver.Chrome(driver_path, options=chrome_options) # Initiliserar chrome drivern
+        self.browser = webdriver.Chrome(driver_path, options=chrome_options) # Initiliserar chrome drivern från den nerladdade
         self.addCleanup(self.browser.quit) # stäng webläsaren när testen är klar
 
-        self.website_url = "https://ntig-uppsala.github.io/pizzeria-rafiki/"
+        self.website_url = "https://ntig-uppsala.github.io/pizzeria-rafiki/" # Urln som kommer användas
 
     # Test som kollar ifall Pizzeria rafiki finns med i titeln på sidan pizzeria-rafiki.github.io
     def test_page_title(self):
         self.browser.get(self.website_url)
-        self.assertIn('Pizzeria Rafiki', self.browser.title)
+        self.assertIn('Pizzeria Rafiki', self.browser.title) # Testa ifall "Pizzera Rafiki" är med i titlen på browsern
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)

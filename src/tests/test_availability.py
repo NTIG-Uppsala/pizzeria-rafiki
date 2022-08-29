c4155d92ac27c81b293c76750a7c6bb902586e8b
Linus J <linus.jansson2000@gmail.com>
2022-08-29 12:55:39 +0200

    Test case för att se ifall hemsidan är live på internet
    - detta bör fungera som en github action som körs varje gång en push & pull_request händer på repot

---
+++ src/tests/test_availability.py
@@ -0,0 +1,27 @@
+# Utgår ifrån https://github.com/jsoma/selenium-github-actions
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+
+
+class CheckSiteAvailability(unittest.TestCase):
+    def setUp(self):
+        driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+
+        chrome_options = Options()
+        chrome_options.add_argument("--headless") #
+
+        self.browser = webdriver.Chrome(driver_path, options=chrome_options) # Initiliserar chrome drivern
+        self.addCleanup(self.browser.quit) # stäng webläsaren när testen är klar
+
+        self.website_url = "https://ntig-uppsala.github.io/pizzeria-rafiki/"
+
+    # Test som kollar ifall Pizzeria rafiki finns med i titeln på sidan pizzeria-rafiki.github.io
+    def test_page_title(self):
+        self.browser.get(self.website_url)
+        self.assertIn('Pizzeria Rafiki', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)

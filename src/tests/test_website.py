c661c5aea4414c752f095a599a816d44ca9b286f
Linus J <linus.jansson2000@gmail.com>
2022-08-29 15:03:22 +0200

    La till tester för företags info
    la till företags info på hemsidan + tester för det

--- src/tests/test_website.py
+++ src/tests/test_website.py
@@ -4,9 +4,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.common.by import By
 
 
 class CheckSiteAvailability(unittest.TestCase):
+    """
+        Om skriptet failar första test ("test_page_title")
+        så betyder det att hemsidan inte är online och körs
+    """
     def setUp(self):
         driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install() # installerar drivern i minnet
 
@@ -17,11 +22,29 @@ class CheckSiteAvailability(unittest.TestCase):
         self.addCleanup(self.browser.quit) # stäng webläsaren när testen är klar
 
         self.website_url = "https://ntig-uppsala.github.io/pizzeria-rafiki/src/" # Urln som kommer användas
+        # self.website_url = "http://127.0.0.1:5500/src/" # Urln som kommer användas
+
 
     # Test som kollar ifall Pizzeria rafiki finns med i titeln på sidan pizzeria-rafiki.github.io
     def test_page_title(self):
         self.browser.get(self.website_url)
         self.assertIn('Pizzeria Rafiki', self.browser.title) # Testa ifall "Pizzera Rafiki" är med i titlen på browsern
+
+    def test_check_for_company_info(self):
+        information = {
+            "company_name": "Pizzeria Rafiki",
+            "telefon_number": "123-456 78 90",
+            "adress": "Flenvägen 55",
+            "open_hours": "Öppet 11-22",
+            "mail_adress": "kontakt@rafiki.se"
+        }
+
+        self.browser.get(self.website_url)
+
+        for info_value in information.values():
+            self.assertIn(info_value, self.browser.find_element(By.TAG_NAME, "body").text)
+
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)

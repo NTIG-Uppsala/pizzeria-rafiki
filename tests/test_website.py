68f1a843e8d83688cad1175ce53872daba22fcea
Linus J <linus.jansson2000@gmail.com>
2022-09-01 11:49:55 +0200

    changed to testing on firefox

--- tests/test_website.py
+++ tests/test_website.py
@@ -1,12 +1,11 @@
 # Utgår ifrån https://github.com/jsoma/selenium-github-actions
 import unittest
 import sys
-import re
 from pathlib import Path
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.common.by import By
 
 class CheckSiteAvailability(unittest.TestCase):
@@ -17,13 +16,12 @@ class CheckSiteAvailability(unittest.TestCase):
     website_url = "http://localhost:8000/" # Standard URL placeholder 
 
     def setUp(self):
-        driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install() # Initializes the driver to memory
 
         # Run the browser with no GUI as it needs to be able to run as a github action
-        chrome_options = Options()
-        chrome_options.add_argument("--headless") 
+        run_options = FirefoxOptions()
+        run_options.add_argument("--headless") 
 
-        self.browser = webdriver.Chrome(driver_path, options=chrome_options) # Initializes the browser instance with the driver
+        self.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=run_options) # Initializes the browser instance with the driver
         self.addCleanup(self.browser.quit) # Closes browser instance when tests are done
 
     # Test to check if "pizzeria rafiki" is in the <title> of the page
@@ -138,6 +136,17 @@ class CheckSiteAvailability(unittest.TestCase):
         # Check each values from the dict if they are present on the webpage
         for product in products:
             self.assertIn(product, products_table)
+
+    def test_check_for_logo(self):
+        self.browser.get(self.website_url)
+
+        # Gets header logo element and favicon element
+        favicon_element = self.browser.find_element(By.XPATH, "xpath[@rel='icon']")
+        header_icon_element = self.browser.find_element(By.ID, "HeaderLogo")
+
+        # Checks if correct logo file is in src and href
+        self.assertIn('rafikilogofavicon.png', favicon_element.get_attribute('href'))
+        self.assertIn('rafikilogo.png', header_icon_element.get_attribute('src'))
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:

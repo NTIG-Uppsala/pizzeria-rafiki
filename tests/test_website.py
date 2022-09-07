eee63140b628ffb5aa2c7001cda18184c1fe7b98
Linus J <linus.jansson2000@gmail.com>
2022-09-07 13:23:35 +0200

    refactor of tests
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>
    Co-Authored-By: Leo Kempe <leo.kempe@elev.ntig.se>

--- tests/test_website.py
+++ tests/test_website.py
@@ -9,6 +9,9 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 class CheckSiteAvailability(unittest.TestCase):
     """
         Class to handle the tests for the website
@@ -36,79 +39,48 @@ class CheckSiteAvailability(unittest.TestCase):
         self.browser.get(self.website_url)
         self.assertIn('Pizzeria Rafiki', self.browser.title) 
 
-    def test_check_for_company_info(self):
-        information = {
-            "company_name": "Pizzeria Rafiki",
-            "phone_number": "Telefonnummer: 0630-555-555",
-            "adress": "Adress: Fjällgatan 32H 981 39 Flen",
-            "open": "Öppet:",
-            "monday": "Måndagar 10-22",
-            "tuesday": "Tisdagar 10-22",
-            "wednesday": "Onsdagar 10-22",
-            "thusday": "Torsdagar 10-22",
-            "friday": "Fredagar 10-23",
-            "saturday": "Lördagar 12-23",
-            "sunday": "Söndagar 12-20",
-            "mail_adress": "Mailadress: info@rafiki.se"
-        }
+    # def test_check_for_company_info(self):
+    #     information = {
+    #         "company_name": "Pizzeria Rafiki",
+    #         "phone_number": "Telefonnummer: 0630-555-555",
+    #         "adress": "Adress: Fjällgatan 32H 981 39 Flen",
+    #         "open": "Öppet:",
+    #         "monday": "Måndagar 10-22",
+    #         "tuesday": "Tisdagar 10-22",
+    #         "wednesday": "Onsdagar 10-22",
+    #         "thusday": "Torsdagar 10-22",
+    #         "friday": "Fredagar 10-23",
+    #         "saturday": "Lördagar 12-23",
+    #         "sunday": "Söndagar 12-20",
+    #         "mail_adress": "Mailadress: info@rafiki.se"
+    #     }
 
-        self.browser.get(self.website_url)
+    #     self.browser.get(self.website_url)
 
-        body_text = self.browser.find_element(By.TAG_NAME, "body").text # Grab all text from the page body
+    #     body_text = self.browser.find_element(By.TAG_NAME, "body").text # Grab all text from the page body
 
-        # Check each values from the dict if they are present on the webpage
-        for info_value in information.values():
-            self.assertIn(info_value, body_text)
+    #     # Check each values from the dict if they are present on the webpage
+    #     for info_value in information.values():
+    #         self.assertIn(info_value, body_text)
+    
+    def check_image(self, x):
+        background_element = self.browser.find_element(By.CLASS_NAME, "Background")
+        css_element = background_element.value_of_css_property("background-image")
+        
+        return "background{}.jpg".format(x) in css_element
+
 
     def test_for_background(self):
         self.browser.get(self.website_url)
-        
         css_background_value = self.browser.find_element(By.CLASS_NAME, "Background")
-
-        for _ in range(1, 3):    
-            time.sleep(1)
-            css_properties = css_background_value.value_of_css_property("background-image")
-
-            self.assertIn(f"background{_}", css_properties)
         
-        
-        
-        # javascript_script = """
-        # """
+        wait = WebDriverWait(self.browser, 10)
+        check_image_lambda = lambda _ : self.check_image(str(i))
 
-        # self.browser.execute_script("console.log('test')")
-
-        # print(self.browser.get_log('browser'))
-
-
-
-
-    # Test to check for background image in css
-    def test_check_for_background_image(self):
-        self.browser.get(self.website_url)
-        
-        css_background_value = self.browser.find_element(By.CLASS_NAME, "HeaderImage")
-
-        # test if background.jpg is in the value of css property background-image
-        self.assertIn('background.jpg', css_background_value.value_of_css_property("background-image") ) 
-
-    def test_check_for_product_image1(self):
-        self.browser.get(self.website_url)
-        
-        # Locate element and get its product image value
-        product_element1 = self.browser.find_element(By.XPATH, '//img[@src="assets/images/prod1.jpg"]')
-
-        # test if prod1.jpg is on the website 
-        self.assertIn('prod1.jpg', product_element1.get_attribute('src'))
-
-    def test_check_for_product_image2(self):
-        self.browser.get(self.website_url)
-        
-        
-        product_element2 = self.browser.find_element(By.XPATH, '//img[@src="assets/images/prod2.jpg"]')
-
-       
-        self.assertIn('prod2.jpg', product_element2.get_attribute('src'))
+        for i in range(1, 4):
+            wait.until(check_image_lambda, "Timeout")
+            print("Wait done")   
+            
 
     def read_svg_data(self, file_name):
         # Get the path to the file
@@ -122,6 +94,7 @@ class CheckSiteAvailability(unittest.TestCase):
             return file_data
 
     # Three tests to check if the correct icons are used on the webpage
+    # TODO: HREF CHECK
     def test_check_for_facebook_icon(self):
         self.browser.get(self.website_url)
         facebook_css_element = self.browser.find_element(By.CLASS_NAME, "FacebookIcon")
@@ -145,7 +118,8 @@ class CheckSiteAvailability(unittest.TestCase):
         file_data = self.read_svg_data('twitter-line.svg')
 
         self.assertIn(file_data, twitter_css_element.value_of_css_property("background-image").replace('\\', ''))
-           
+    
+    # TODO: FIX THIS
     def test_check_for_products(self):
         self.browser.get(self.website_url)
 
@@ -173,8 +147,7 @@ class CheckSiteAvailability(unittest.TestCase):
                 continue
             
             if pizza in listing_text:
-                for key, value in products.items():
-                    self.assertIn(" ".join(value), listing_text)
+                self.assertIn(" ".join(products[pizza]), listing_text)
                 # self.assertIn(products[pizza], listing_text)
 
             print(pizza, type(pizza))

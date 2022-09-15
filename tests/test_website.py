f4742f9aef94bbc831016ca7566b9fd3ad40d337
VincentKarm <105645043+VincentKarm@users.noreply.github.com>
2022-09-15 12:00:24 +0200

    enforced coding standard
    Co-Authored-By: ahedenskog <111979841+ahedenskog@users.noreply.github.com>

--- tests/test_website.py
+++ tests/test_website.py
@@ -1,10 +1,9 @@
 # Utgår ifrån https://github.com/jsoma/selenium-github-actions
-from datetime import datetime, timedelta
+from datetime import datetime
 from types import NoneType
 import unittest
 import sys
 from datetime import datetime
-import time
 import re
 from pathlib import Path
 from selenium import webdriver
@@ -30,9 +29,11 @@ class CheckSiteAvailability(unittest.TestCase):
         
         path_to_binaries = Path(__file__).resolve().parents[1] / Path('bin')
         if sys.platform == "win32":
-            self.browser = webdriver.Firefox(service=FirefoxService(executable_path=path_to_binaries/Path('geckodriver.exe')), options=run_options) # Initializes the browser instance with the driver
+            # Initializes the browser instance with the driver
+            self.browser = webdriver.Firefox(service=FirefoxService(executable_path=path_to_binaries/Path('geckodriver.exe')), options=run_options)
         else:
-            self.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=run_options) # Initializes the browser instance with the driver
+            # Initializes the browser instance with the driver
+            self.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=run_options)
 
         self.addCleanup(self.browser.quit) # Closes browser instance when tests are done
 
@@ -41,25 +42,26 @@ class CheckSiteAvailability(unittest.TestCase):
         self.browser.get(self.website_url)
         self.assertIn('Pizzeria Rafiki', self.browser.title) 
 
+    # Test for contact information
     def test_check_for_contact(self):
         information = {
             "phone_number": "0630-555-555",
             "adress": "Fjällgatan 32H 981 39 Flen",
             "mail_adress": "info@rafiki.se"
         }
-        
         self.browser.get(self.website_url)
         
         self.assertIn('mailto:info@rafiki.se', self.browser.find_element(By.ID, "MailAdress").get_attribute("href"))
         self.assertIn('tel:0630-555-555', self.browser.find_element(By.ID, "PhoneNumber").get_attribute("href"))
 
-        body_text = self.browser.find_element(By.TAG_NAME, "body").text.replace('\n', ' ') # Grab all text from the page body
+        # Grab all text from the page body
+        body_text = self.browser.find_element(By.TAG_NAME, "body").text.replace('\n', ' ') 
 
         # Check each values from the dict if they are present on the webpage
         for info_value in information.values():
             self.assertIn(info_value, body_text)
 
-    
+    # Test for open hours
     def test_check_for_open_hours(self):
         self.browser.get(self.website_url)
 
@@ -74,9 +76,9 @@ class CheckSiteAvailability(unittest.TestCase):
             "Sunday": ["Söndagar", "12-20"],
         }
 
+        # checks open hours
         open_hours_table = self.browser.find_element(By.CLASS_NAME, "Openhours")
         open_hours_elements = open_hours_table.find_elements(By.TAG_NAME, "tr")
-
         for open_hour in open_hours_elements:
             current_day = open_hour.get_attribute("data-day")
             listing_text = open_hour.text
@@ -84,12 +86,12 @@ class CheckSiteAvailability(unittest.TestCase):
             # If a tr has no data-day attribute skip it
             if isinstance(current_day, NoneType):
                 continue
-            
             if current_day in listing_text:
                 self.assertIn(" ".join(open_hours[current_day]), listing_text)
 
     
     def check_image(self, x):
+
         # get the elment with the Background class name
         background_element = self.browser.find_element(By.CLASS_NAME, "Background")
 
@@ -102,40 +104,32 @@ class CheckSiteAvailability(unittest.TestCase):
 
     def test_for_background(self):
         self.browser.get(self.website_url)
-        
-        # Initilize a WebDriverWait object passing in the browser driver and a 30 sec timeout
-        wait = WebDriverWait(self.browser, 30)
-        
-        # lambda function to call the check_image method
-        check_image_lambda = lambda _ : self.check_image(str(i))
 
         # Itterate through all images 1-3 to check if they are shown on the page
         for i in range(1, 4):
-            wait.until(check_image_lambda, "Timeout")
-            print("Wait done")   
-            
+            self.check_image(str(i))      
 
     def read_svg_data(self, file_name):
+
         # Get the path to the file
         icon_path = Path(__file__).resolve().parents[1] / Path(f'src/assets/images/{file_name}')
         
         # Reads the svg data from the the icon_path
         with open(icon_path, 'r+') as file:
+
             # Converts the data to one line, replacing newlines and multiple whitespaces with no space
             file_data = ''.join(file.readlines()).replace('\n', '')
-            
             return file_data
 
     # Three tests to check if the correct icons are used on the webpage    
     def test_check_for_socials(self):
         self.browser.get(self.website_url)
-
         icons = [
             ['FacebookIcon', 'facebook-circle-fill.svg'],
             ['InstagramIcon', 'instagram-fill.svg'],
             ['TwitterIcon', 'twitter-fill.svg']
         ]
-
+        
         # For each icon in the icons list
         for icon in icons:
             print("current icon check:", icon[0])
@@ -166,12 +160,12 @@ class CheckSiteAvailability(unittest.TestCase):
             "Pompei": ["bacon, rödlök, ägg, curry", "90 kr"],
             "La Casa": ["champinjoner, räkor, skinka", "95 kr"]
         }
-
         products_table = self.browser.find_element(By.ID, "Products")
         page_products_element = products_table.find_elements(By.TAG_NAME, "tr")
 
         # Loop through the products table element
         for product in page_products_element:
+
             # Get the data-pizza attribute value
             pizza = product.get_attribute("data-pizza")
             listing_text = product.text
@@ -179,7 +173,7 @@ class CheckSiteAvailability(unittest.TestCase):
             # If the data attribute is a None Type, skip to the next element
             if isinstance(pizza, NoneType):
                 continue
-            
+
             # Compare the Dictionary to the page value
             if pizza in listing_text:
                 self.assertIn(" ".join(products[pizza]), listing_text)
@@ -196,9 +190,10 @@ class CheckSiteAvailability(unittest.TestCase):
         self.assertIn('rafikilogo.svg', header_icon_element.value_of_css_property('background-image'))
 
     def test_for_large_images(self):
+
         # Get path for image folder
         image_path = Path(__file__).resolve().parents[1] / Path('src/assets/images/')
-        
+
         # Assert check for images larger than 500kB
         for image in image_path.glob('**/*.*'):
             image_size = Path(image).stat().st_size
@@ -207,7 +202,6 @@ class CheckSiteAvailability(unittest.TestCase):
 
     def test_screenshot(self):
         self.browser.get(self.website_url)
-    
         resolutions = [
             [2560, 1440], # 2k desktop
             [1920, 1080], # desktop
@@ -215,16 +209,12 @@ class CheckSiteAvailability(unittest.TestCase):
             [820, 1180], # iPad Air
             [390, 844], # iPhone 12 Pro
         ]
-
         for res in resolutions:
             x,y = res
-
             date = str(datetime.now())
-
             self.browser.set_window_position(0, 0)
             self.browser.set_window_size(x, y)
             self.browser.save_screenshot("screenshots/screenshot" + re.sub(r'[\W_]+', '', date) + str(x) + "x" + str(y) + ".png")
-            
             print("saved screenshot with resolution", x, y)
 
     def test_map(self):

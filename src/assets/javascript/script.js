31115ceefe776f6b3ba0356a47155e216997718b
VincentKarm <105645043+VincentKarm@users.noreply.github.com>
2022-09-20 13:53:09 +0200

    fixed the input field
    Co-Authored-By: lukas-laas <111971517+lukas-laas@users.noreply.github.com>

--- src/assets/javascript/script.js
+++ src/assets/javascript/script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", (event) =>
     let htmlIframeString = `<iframe id="MapInteractive" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2051.9318612247876!2d16.592368716487428!3d59.04985334044074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465ec4abc74e82c5%3A0x4642c0225f90c202!2sKungsv%C3%A4gen%202%2C%20642%2034%20Flen!5e0!3m2!1ssv!2sse!4v1663054178090!5m2!1ssv!2sse" style="border: 0; width: 100%; height: 300px;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>`
     document.querySelector("#mapadress").innerHTML += htmlIframeString
 
-    let htmlZipcodeCheck = '<p>Skriv ditt postnummer för att se om vi kör ut till dig!</p><form action=""><input type="number" id="number"><input id="submit" type="submit" value="Submit"></form>'
+    let htmlZipcodeCheck = '<p>Skriv ditt postnummer för att se om vi kör ut till dig!</p><form action=""><input type="number" id="number" min="10000" max="99999" placeholder="64230"><input id="submit" type="submit" value="Submit"></form><p id="output"></p>'
     document.querySelector("#jsCheck").innerHTML = htmlZipcodeCheck
 
     document.querySelector("#postnummerCheck form").addEventListener("submit", (event) => {
@@ -29,13 +29,13 @@ document.addEventListener("DOMContentLoaded", (event) =>
         // 
         if (event.submitter.parentNode.querySelector("#number").value.length != 5)
         {
-            document.querySelector("#postnummerCheck p").innerHTML = "Inte ett postnummer."
+            document.querySelector("#output").innerHTML = "Inte ett postnummer."
         }
         else if(zipCodeList.includes(event.submitter.parentNode.querySelector("#number").value)){
-            document.querySelector("#postnummerCheck p").innerHTML = "Vi kör ut, ring telefonnummret ovan!"
+            document.querySelector("#output").innerHTML = "Vi kör ut, ring telefonnummret ovan!"
         }
         else{
-            document.querySelector("#postnummerCheck p").innerHTML = "Vi kör tyvärr inte ut till dig."
+            document.querySelector("#output").innerHTML = "Vi kör tyvärr inte ut till dig."
         }
     })
 })

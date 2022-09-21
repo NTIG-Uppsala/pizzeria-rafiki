e0fba81cf77fa375de77469920890b3fea155c25
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2022-09-21 13:12:34 +0000

    Deploying to gh-pages from @ NTIG-Uppsala/pizzeria-rafiki@20fc94e01e5bddd8ae2cfc8daa19053e2d90bbfd 🚀

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", (event) =>
     let htmlIframeString = `<iframe id="MapInteractive" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2051.9318612247876!2d16.592368716487428!3d59.04985334044074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465ec4abc74e82c5%3A0x4642c0225f90c202!2sKungsv%C3%A4gen%202%2C%20642%2034%20Flen!5e0!3m2!1ssv!2sse!4v1663054178090!5m2!1ssv!2sse" style="border: 0; width: 100%; height: 300px;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>`
     document.querySelector("#mapadress").innerHTML += htmlIframeString
 
-    let htmlZipcodeCheck = '<p>Skriv ditt postnummer för att se om vi kör ut till dig!</p><form action=""><input type="number" id="number" placeholder="64230"><input id="submit" type="submit" value="Submit"></form><p id="output"></p>'
+    let htmlZipcodeCheck = '<p>Skriv ditt postnummer för att se om vi kör ut till dig!</p><form action=""><input type="text" inputmode="numeric" id="number" placeholder="64230"><input id="submit" type="submit" value="Kolla"></form><p id="output"></p>'
     document.querySelector("#jsCheck").innerHTML = htmlZipcodeCheck
 
     // console.log(d, day, time, openHours[day][0]);

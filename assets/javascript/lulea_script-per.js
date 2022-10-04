db0de5cecc1dd593f176a34d0bc8dd31bf3f7a92
Hilmir4 <111963441+Hilmir4@users.noreply.github.com>
2022-10-04 12:30:08 +0000

    Deploying to gh-pages from @ NTIG-Uppsala/pizzeria-rafiki@69ec04df7410a3274d545e118ad85bc7b8236e64 🚀

--- assets/javascript/lulea_script-per.js
+++ assets/javascript/lulea_script-per.js
@@ -25,7 +25,7 @@ let OpenSign = null;
 
 document.addEventListener("DOMContentLoaded", (event) => 
 {
-    let htmlIframeString = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1643.0857221286803!2d21.84887801662537!3d65.68080409348582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x467f677c34b6b1af%3A0x493f441e2dee92f!2sF%C3%A4rjledsv%C3%A4gen%2038%2C%20961%2093%20S%C3%B6dra%20Sunderbyn!5e0!3m2!1ssv!2sse!4v1664867957394!5m2!1ssv!2sse"  style="border: 0; width: 100%; height: 300px;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+    let htmlIframeString = `<iframe id="MapInteractive" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1643.0857221286803!2d21.84887801662537!3d65.68080409348582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x467f677c34b6b1af%3A0x493f441e2dee92f!2sF%C3%A4rjledsv%C3%A4gen%2038%2C%20961%2093%20S%C3%B6dra%20Sunderbyn!5e0!3m2!1ssv!2sse!4v1664867957394!5m2!1ssv!2sse"  style="border: 0; width: 100%; height: 300px;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>`
     document.querySelector("#mapadress").innerHTML += htmlIframeString
 
     let htmlZipcodeCheck = '<p>کد پستی خود را وارد کنید تا ببینید آیا ما به شما تحویل می دهیم یا خیر!</p><form action=""><input type="text" inputmode="numeric" id="number" placeholder="961 90"><input class="checkNumber" id="submit" type="submit" value="بررسی"></form><p id="output"></p>'
@@ -57,16 +57,16 @@ document.addEventListener("DOMContentLoaded", (event) =>
         zipInput = zipInput.split(" ").join("") //removes spaces from string
 
         if (zipInput.match(/\D/) != null) {
-            document.querySelector("#output").innerHTML = "Inte ett postnummer."
+            document.querySelector("#output").innerHTML = "کد پستی نیست"
         }
         else if (zipInput.length != 5) {
-            document.querySelector("#output").innerHTML = "Inte ett postnummer."
+            document.querySelector("#output").innerHTML = "کد پستی نیست"
         }
         else if(zipCodeList.includes(zipInput)) {
-            document.querySelector("#output").innerHTML = "Vi kör ut, ring telefonnumret ovan!"
+            document.querySelector("#output").innerHTML = "ما به سمت شما رانندگی می کنیم، با شماره تلفن بالا تماس بگیرید"
         }
         else {
-            document.querySelector("#output").innerHTML = "Vi kör tyvärr inte ut till dig."
+            document.querySelector("#output").innerHTML = "متأسفانه ما به شما رانندگی نمی کنیم."
         }
     })
 })
